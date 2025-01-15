a964d209cf7a2a952831a53838ae6e83a5a514cc
Yucel Okcu <yucel.n.okcu@gmail.com>
2025-01-15 14:13:57 +0300

    chore: initial commit

---
+++ src/main/java/com/example/yuceldemo/YucelDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.yuceldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YucelDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YucelDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yuceldemo/YucelDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yuceldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YucelDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
